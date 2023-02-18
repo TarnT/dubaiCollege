01fea3efc6b562568cf9e3f50dae5527e89927c9
Tarn <timmermanstarn@gmail.com>
2023-02-18 14:14:46 +0400

    deleted old code

--- learnFlask/app.py
+++ learnFlask/app.py
@@ -10,28 +10,5 @@ app = Flask(__name__)
 app.register_blueprint(general_bp)
 app.register_blueprint(auth_bp)
 
-
-'''
-@app.route("/login", methods=["GET", "POST"])
-def login():
-
-    if request.method == "POST":
-        email = request.form.get("inputEmail")
-        password = request.form.get("inputPassword")
-        print(email, password)
-
-    return render_template("login.html")
-
-@app.route("/sign-up", methods=["GET", "POST"])
-def signUp():
-
-    if request.method == "POST":
-        email = request.form.get("inputEmail")
-        password1 = request.form.get("inputPassword1")
-        password2 = request.form.get("inputPassword2")
-
-    return render_template("signUp.html")
-'''
-
 if __name__ == "__main__":
     app.run(debug=True)

7e1c8a65e7f1f8580deac0a59afe71628718f195
Tarn <timmermanstarn@gmail.com>
2023-02-18 15:09:55 +0400

    configured db in app.py created db in root directory

--- learnFlask/app.py
+++ learnFlask/app.py
@@ -7,6 +7,12 @@ from auth.auth import auth_bp
 # CONFIG
 app = Flask(__name__)
 app.config["EXPLAIN_TEMPLATE_LOADING"] = True
+db = SQLAlchemy()
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 # registering blueprints
 app.register_blueprint(general_bp)
--- learnFlask/models.py
+++ learnFlask/models.py
@@ -1,1 +1,8 @@
- 
+from app import db 
+from flask_login import UserMixin
+from sql_alchemy.sql import func
+
+class User(db.model):
+    id = db.Column(db.integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.string, nullable=False)
+    password = db.Column(db.string, nullable=False)

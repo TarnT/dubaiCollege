43c70845fac3eb6b65dd62e02ca934a7dc98ead6
Tarn <timmermanstarn@gmail.com>
2023-02-18 16:04:48 +0400

    setting up adding user to db

--- learnFlask/auth/auth.py
+++ learnFlask/auth/auth.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash 
+from app import db
+from models import User
 
 auth_bp = Blueprint("auth_bp", __name__, template_folder="templates/auth")
 
@@ -8,7 +10,7 @@ def login():
     if request.method == "POST":
         email = request.form.get("inputEmail")
         password = request.form.get("inputPassword")
-        print(email, password)
+
 
     return render_template("login.html")
 
@@ -19,5 +21,18 @@ def signUp():
         email = request.form.get("inputEmail")
         password1 = request.form.get("inputPassword1")
         password2 = request.form.get("inputPassword2")
+        
+        exists = User.query.filter_by(email=email).first() or False 
+        if exists:
+            flash("Email already exists!", category="error")
+        elif password1 != password2:
+            flash("Passwords don't match!", category="error")
+        else:
+            new_user = User(email=email, password=password1)
+            db.session.add(new_user)
+            db.session.commit()
+
+        # TODO implement checks for passwords and email
+
 
     return render_template("signUp.html")
--- learnFlask/models.py
+++ learnFlask/models.py
@@ -2,9 +2,9 @@ from app import db
 from datetime import datetime
 from flask_login import UserMixin
 
-class User(db.model, UserMixin):
+class User(db.Model, UserMixin):
     __tablename__ = "Users"
-    id = db.Column(db.integer, primary_key=True, autoincrement=True)
-    email = db.Column(db.string, unique=True, nullable=False)
-    password = db.Column(db.string, nullable=False)
-    date_created = db.Column(db.Date(timezone=True), default=datetime.now())
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    date_created = db.Column(db.DateTime(timezone=True), default=datetime.now())

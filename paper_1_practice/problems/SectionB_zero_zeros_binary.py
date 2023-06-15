ffee8b5618f1e07d949c0607752005bccfa7e5be
Tarn <timmermanstarn@gmail.com>
2023-06-15 12:12:59 +0400

    one line madness

--- paper_1_practice/problems/SectionB_zero_zeros_binary.py
+++ paper_1_practice/problems/SectionB_zero_zeros_binary.py
@@ -1,20 +1,5 @@
 def zeroZeroesBinary(num):
 
-    return_num = num
+    return int(f"0{bin(num).replace('0', '')}", base=2) if num > 0 else -1
 
-    if num <= 0:
-        return -1
-    
-    num = str(bin(num)[2::])
-    num = num.replace("0", "")
-
-    multiplier = 1
-    total = 0
-
-    for i in range(0, len(num)):
-        total += multiplier
-        multiplier *= 2
-    
-    return f"The zero zeroes binary number of {return_num} is {total}"
-
-print(zeroZeroesBinary(53178))
+print(zeroZeroesBinary(-10))

57d30e1724826a94ccb9436ec8dcc68a27c43548
Tarn <timmermanstarn@gmail.com>
2023-07-01 07:08:53 +1200

    fixing db

--- learnFlask/models.py
+++ learnFlask/models.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 class User(db.Model, UserMixin):
     __tablename__ = "Users"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(b.String, unique=True)
+    username = db.Column(db.String, unique=True)
     email = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
     date_created = db.Column(db.DateTime(timezone=True), default=datetime.now())

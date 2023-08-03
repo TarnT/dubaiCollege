5eff6eee15d7134d9ff274741452a3c18b907d71
Tarn <timmermanstarn@gmail.com>
2023-08-03 21:10:11 +1200

    lots more html pdf stuff

--- nea_prototyping/pdfs/pdfs.py
+++ nea_prototyping/pdfs/pdfs.py
@@ -8,24 +8,13 @@ def generate_pdf(html_content, output_path):
         # generate PDF
         pdfkit.from_string(html_content, output_path, configuration=config)
         
-        print(f"PDF generated successfully at: {output_path}")
-        
+        print(f"PDF generated!")
+
     except Exception as e:
         print(f"Error generating PDF: {e}")
 
-# Example usage
-html_content = """
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Sample PDF</title>
-</head>
-<body>
-    <h1>Hello, this is a sample PDF generated using Python and HTML!</h1>
-    <p>More content can go here...</p>
-</body>
-</html>
-"""
+with open("pdf_html.html", "r") as html_file:
+    html_content = html_file.read()
 
-output_path = "sample_output.pdf"
+output_path = "test_html_output.pdf"
 generate_pdf(html_content, output_path)

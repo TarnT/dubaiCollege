c48733a45144f80be8dbd0442522879d626d21d8
Tarn <timmermanstarn@gmail.com>
2023-08-05 13:44:18 +1200

    working on image

--- nea_prototyping/pdfs/pdfs.py
+++ nea_prototyping/pdfs/pdfs.py
@@ -1,20 +1,33 @@
 import pdfkit
+from jinja2 import Environment, FileSystemLoader
+import base64
 
-def generate_pdf(html_content, output_path):
+def get_image_file_as_base64_data():
+    with open(FILEPATH, 'rb') as image_file:
+        return base64.b64encode(image_file.read())
+
+def generate_html(template_path, data):
+    env = Environment(loader=FileSystemLoader("."))
+    template = env.get_template(template_path)
+    return template.render(data)
+
+def generate_pdf(input_data, output_path):
     try:
 
         config = pdfkit.configuration(wkhtmltopdf='/usr/local/bin/wkhtmltopdf')
 
         # generate PDF
-        pdfkit.from_string(html_content, output_path, configuration=config)
+        pdfkit.from_string(input_data, output_path, configuration=config)
         
         print(f"PDF generated!")
 
     except Exception as e:
         print(f"Error generating PDF: {e}")
 
-with open("pdf_html.html", "r") as html_file:
-    html_content = html_file.read()
+data = {"title": "This a title using Jinja",
+        "name": "Tarn",
+        "watch_image": image_to_string("watch.jpg")}
 
 output_path = "/Users/tarntimmermans/Documents/repositories/dubaiCollege/nea_prototyping/pdfs/output_pdfs/test.pdf"
-generate_pdf(html_content, output_path)
+html = generate_html("template.html", data)
+generate_pdf(html, "output_pdfs/test.pdf")

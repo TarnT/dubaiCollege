9452382e89b45832a540266311e8de90b3e25975
Tarn <timmermanstarn@gmail.com>
2023-08-05 14:28:01 +1200

    table

--- nea_prototyping/pdfs/pdfs.py
+++ nea_prototyping/pdfs/pdfs.py
@@ -1,10 +1,16 @@
 import pdfkit
-from jinja2 import Environment, FileSystemLoader
 import base64
+import csv
+from jinja2 import Environment, FileSystemLoader
 
 def get_image_file_as_base64_data(path):
     with open(path, 'rb') as image_file:
         return base64.b64encode(image_file.read()).decode('utf-8')
+    
+def get_table_data(path):
+    with open(path) as csv_file:
+        csv_data = csv.DictReader(csv_file)
+        return csv_data
 
 def generate_html(template_path, data):
     env = Environment(loader=FileSystemLoader("."))
@@ -26,7 +32,8 @@ def generate_pdf(input_data, output_path):
 
 data = {"title": "This a title using Jinja",
         "name": "Tarn",
-        "watch_image": get_image_file_as_base64_data("watch.jpg")}
+        "watch_image": get_image_file_as_base64_data("watch.jpg"),
+        "table_data": get_table_data("data.csv")}
 
 html = generate_html("template.html", data)
 generate_pdf(html, "output_pdfs/test.pdf")

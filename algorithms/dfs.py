3b72290d19e67cfb9cd84bfbc36cf7e21aa1252b
TarnT <69193456+TarnT@users.noreply.github.com>
2023-11-14 09:29:41 +0400

    dfs

--- algorithms/dfs.py
+++ algorithms/dfs.py
@@ -1,11 +1,28 @@
-def iterative_depth_first_search(node):
+def depth_first_search(graph, node):
+
     stack = []
-    visited_
+    visited_nodes = []
+
     stack.append(node)
+
     while len(stack) != 0:
-        current = stack.pop()
-        if not current.visited:
-            visit(current)
-            current.visited = True
-            for neighbor in current.neighbors:
-                if not neighbor.visited: stack. append (neighbor)
+        current_node = stack.pop()
+        if current_node not in visited_nodes:
+            visited_nodes.append(current_node)
+            for neighbour_node in graph[current_node]:
+                if neighbour_node not in visited_nodes:
+                    stack.append(neighbour_node)
+
+    return visited_nodes
+
+graph1 = {"A": ["C","B"],
+         "B": ["A","C","D","E"],
+         "C": ["A","B","F","D"],
+         "D": ["C","E","B","H"],
+         "E": ["B","D"],
+         "F":["C","G"],
+         "G":["F"],
+         "H":["D"]
+         }
+
+print(depth_first_search(graph1, "A"))
